8d2944164ef09d104c41947aa947aefd349eaa15
rownok35 <rownokrezwan@gmail.com>
2022-06-15 21:32:21 +0600

    changes in static files

--- studybud/settings.py
+++ studybud/settings.py
@@ -132,7 +132,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'studybud/static'),
 ]
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_ROOT = BASE_DIR / 'static/images'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
